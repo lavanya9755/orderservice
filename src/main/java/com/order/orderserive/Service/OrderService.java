a455cd555b2eef150e4575e38ccf5372ebcdafd7
lavanya9755 <lavanyachawla24@gmail.com>
2025-06-03 01:41:19 +0530

    sync connec-inventoryclient added

--- src/main/java/com/order/orderserive/Service/OrderService.java
+++ src/main/java/com/order/orderserive/Service/OrderService.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import com.order.orderserive.Repository.OrderRepository;
+import com.order.orderserive.client.InventoryClient;
 import com.order.orderserive.dto.OrderRequest;
 import com.order.orderserive.model.Order;
 
@@ -16,22 +17,31 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private InventoryClient inventoryClient;
 
 
     public  void placeOrder(OrderRequest orderRequest){
+        var productInStock = inventoryClient.inStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(productInStock){
         //map order reuqest to order objects 
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setPrice(orderRequest.price());
         order.setSkuCode(orderRequest.skuCode());
         order.setQuantity(orderRequest.quantity());
-
-        //save order object in list(orderrepo)
-        try {
-            orderRepository.save(order);
-        } catch (Exception e) {
-            e.printStackTrace(); // log what exactly is failing
+        orderRepository.save(order);
+        }else{
+            throw new RuntimeException("Product is with skuCode"+ orderRequest.skuCode() + " is not in stock");
         }
+
+        // testing
+        // //save order object in list(orderrepo)
+        // try {
+        //     orderRepository.save(order);
+        // } catch (Exception e) {
+        //     e.printStackTrace(); // log what exactly is failing
+        // }
         
 
 
